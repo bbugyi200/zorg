9ae84ce13d42fd3aeae04fceeb5eadf2a4552559
Bryan Bugyi <bryanbugyi34@gmail.com>
2024-01-21 00:21:16 -0500

    Add support for initial template # comments
    These comments MUST be followed by a blank line and are NOT included in
    generated files (which is the point).

--- src/zorg/runners.py
+++ src/zorg/runners.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 import datetime as dt
 from functools import partial
 from pathlib import Path
+import tempfile
 from typing import Iterable, Iterator, List
 
 from clack.types import ClackRunner
@@ -25,8 +26,19 @@ logger = Logger(__name__)
 @runner
 def run_day(cfg: DayConfig) -> int:
     """Runner for the 'day' command."""
-    template_loader = jinja2.FileSystemLoader(searchpath=cfg.zettel_dir)
+    temp_dir = tempfile.TemporaryDirectory()
+    temp_dir_path = Path(temp_dir.name)
+    template_loader = jinja2.FileSystemLoader(searchpath=temp_dir_path)
     template_env = jinja2.Environment(loader=template_loader)
+    _build_template_in_dir(
+        temp_dir_path, cfg.zettel_dir / cfg.day_log_template
+    )
+    _build_template_in_dir(
+        temp_dir_path, cfg.zettel_dir / cfg.habit_log_template
+    )
+    _build_template_in_dir(
+        temp_dir_path, cfg.zettel_dir / cfg.done_log_template
+    )
     day_log_template = template_env.get_template(cfg.day_log_template)
     habit_log_template = template_env.get_template(cfg.habit_log_template)
     done_log_template = template_env.get_template(cfg.done_log_template)
@@ -57,6 +69,23 @@ def run_day(cfg: DayConfig) -> int:
             commands=_process_vim_commands(cfg.zettel_dir, cfg.vim_commands),
         )
     return 0
+
+
+def _build_template_in_dir(temp_dir: Path, template_path: Path) -> None:
+    temp_template_path = temp_dir / template_path.name
+    new_lines = []
+    blank_line_found = False
+    for line in template_path.open("r"):
+        if not blank_line_found and not line.strip():
+            blank_line_found = True
+            continue
+        if blank_line_found:
+            new_lines.append(
+                line[1:]
+                if line.startswith("## ") or line.strip() == "##"
+                else line
+            )
+    temp_template_path.write_text("".join(new_lines))
 
 
 def _ensureDayLogFileExists(
