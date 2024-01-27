fb25b5d789256bf9a9791a75aff1ce57aae9e6da
Bryan Bugyi <bryanbugyi34@gmail.com>
2024-01-27 12:18:44 -0500

    Blacken tests/

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 """This file contains shared fixtures and pytest hooks.
 
 https://docs.pytest.org/en/6.2.x/fixture.html#conftest-py-sharing-fixtures-across-multiple-files
+
 """
 
 from __future__ import annotations
