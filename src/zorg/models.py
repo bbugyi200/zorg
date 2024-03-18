8042ce4d3e811d2b0a4c62073bc7108f2e7d71ab
Bryan Bugyi <bryanbugyi34@gmail.com>
2024-03-18 10:43:30 -0400

    Rename to ZorgNote and ZorgTodo

--- src/zorg/models.py
+++ src/zorg/models.py
@@ -9,26 +9,26 @@ from .types import TodoPriorityType, TodoStatus
 
 
 @dataclass(frozen=True)
-class Note:
+class ZorgNote:
     """A Zorg note."""
 
     body: str
     file: Path
 
-    child_notes: list["Note"] = field(default_factory=lambda: [])
+    child_notes: list["ZorgNote"] = field(default_factory=lambda: [])
     context_tags: list[str] = field(default_factory=lambda: [])
     create_date: dt.date = dt.date.today()
-    next_note: Optional["Note"] = None
-    parent_note: Optional["Note"] = None
+    next_note: Optional["ZorgNote"] = None
+    parent_note: Optional["ZorgNote"] = None
     people_tags: list[str] = field(default_factory=lambda: [])
-    prev_note: Optional["Note"] = None
+    prev_note: Optional["ZorgNote"] = None
     project_tags: list[str] = field(default_factory=lambda: [])
     properties: dict[str, str] = field(default_factory=lambda: {})
     role_tags: list[str] = field(default_factory=lambda: [])
 
 
 @dataclass(frozen=True)
-class Todo(Note):
+class ZorgTodo(ZorgNote):
     """A Zorg todo."""
 
     done_date: Optional[dt.date] = None
