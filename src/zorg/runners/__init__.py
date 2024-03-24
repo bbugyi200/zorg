f849f6564e491ec1712dfb384e318e25eba3a040
Bryan Bugyi <bryanbugyi34@gmail.com>
2024-03-24 10:12:07 -0400

    Add comments to runners/__init__.py

--- src/zorg/runners/__init__.py
+++ src/zorg/runners/__init__.py
@@ -1,9 +1,16 @@
-"""Contains this project's clack runners."""
+"""Contains this project's clack runners.
+
+WARNING: This package should contain modules of the form _runners.py or
+_run_*.py ONLY. Do not be tempted to add other libraries to this package even
+if they are only used by runners.
+"""
 
 from . import _run_action, _run_compile, _run_db, _run_edit, _run_template
 from ._runners import RUNNERS
 
 
+# HACK: It is necessary that we import these runner modules so the '@runner'
+# decorator registers functions as runners.
 del _run_action, _run_compile, _run_db, _run_edit, _run_template
 
 __all__ = ["RUNNERS"]
