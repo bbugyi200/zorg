e65aecdf15a5997f9a34b9e686133fc59b04ca4b
Bryan Bugyi <bryanbugyi34@gmail.com>
2024-04-03 18:26:34 -0400

    Fix mypy lint errors

--- src/zorg/model_converter.py
+++ src/zorg/model_converter.py
@@ -1,19 +1,29 @@
 """Contains logic to convert domain models to/from SQL models."""
 
+from pathlib import Path
+
 from . import models, sql_models
 
 
 def file_to_sql_model(model: models.ZorgFile) -> sql_models.ZorgFile:
     """Model-to-SQL-model converter for a ZorgFile."""
+    del model
+    return sql_models.ZorgFile()
 
 
 def sql_file_to_model(sql_model: sql_models.ZorgFile) -> models.ZorgFile:
     """Model-to-SQL-model converter for a ZorgFile."""
+    del sql_model
+    return models.ZorgFile(Path("."))
 
 
 def note_to_sql_model(model: models.ZorgNote) -> sql_models.ZorgNote:
     """Model-to-SQL-model converter for a ZorgFile."""
+    del model
+    return sql_models.ZorgNote(desc="")
 
 
 def sql_note_to_model(sql_model: sql_models.ZorgNote) -> models.ZorgNote:
     """Model-to-SQL-model converter for a ZorgFile."""
+    del sql_model
+    return models.ZorgNote("")
--- src/zorg/sql_models.py
+++ src/zorg/sql_models.py
@@ -112,9 +112,8 @@ class ZorgNote(Base, table=True):
     """Model class for zorg notes."""
 
     # table columns
-    create_date: dt.date
     desc: str
-    done: bool
+    create_date: dt.date = Field(default_factory=dt.date.today)
     todo_priority: Optional[str] = None
     todo_status: Optional[TodoStatus] = None
 
