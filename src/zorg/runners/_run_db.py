3989c3d5322f1a70d02b9667ee3b12c9be757da5
Bryan Bugyi <bryanbugyi34@gmail.com>
2024-04-04 00:43:58 -0400

    Clean up run_db_create()

--- src/zorg/runners/_run_db.py
+++ src/zorg/runners/_run_db.py
@@ -17,6 +17,8 @@ logger = Logger(__name__)
 def run_db_create(cfg: DbCreateConfig) -> int:
     """Runner for the 'db create' command."""
     zorg_files = []
+    total_num_notes = 0
+    total_num_todos = 0
     for zo_path in tqdm(
         sorted(cfg.zettel_dir.rglob("*.zo"), key=lambda p: p.name),
         desc="Reading notes from zorg files",
@@ -24,22 +26,23 @@ def run_db_create(cfg: DbCreateConfig) -> int:
     ):
         logger.info("Starting to walk zorg file", zorg_file=zo_path.name)
         zorg_file = walk_zorg_file(zo_path)
+        num_notes = len(zorg_file.notes)
+        num_todos = len(
+            [note for note in zorg_file.notes if note.todo_payload]
+        )
+        total_num_notes += num_notes
+        total_num_todos += num_todos
         logger.info(
             "Finished walking zorg file",
             zorg_file=zo_path.name,
-            num_notes=len(zorg_file.notes),
-            num_todos=len(
-                [note for note in zorg_file.notes if note.todo_payload]
-            ),
+            num_notes=num_notes,
+            num_todos=num_todos,
         )
         zorg_files.append(zorg_file)
     logger.info(
         "Finished reading zettel org directory",
         num_files=len(zorg_files),
-        num_notes=sum(len(zf.notes) for zf in zorg_files),
-        num_todos=sum(
-            len([note for note in zf.notes if note.todo_payload])
-            for zf in zorg_files
-        ),
+        num_notes=total_num_notes,
+        num_todos=total_num_todos,
     )
     return 0
