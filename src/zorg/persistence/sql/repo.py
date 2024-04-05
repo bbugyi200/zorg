76050ea0ab2dd22c67bf7fe3901ae2b58cc07698
Bryan Bugyi <bryanbugyi34@gmail.com>
2024-04-05 10:52:14 -0400

    Use ZorgFile as aggregate

--- src/zorg/persistence/sql/repo.py
+++ src/zorg/persistence/sql/repo.py
@@ -8,13 +8,13 @@ from potoroo import QueryRepo
 from sqlmodel import Session, select
 
 from . import converters, models as sql
-from ...domain.models import OrZorgQuery, ZorgNote
+from ...domain.models import OrZorgQuery, ZorgFile
 
 
 logger = Logger(__name__)
 
 
-class ZorgSQLRepo(QueryRepo[str, ZorgNote, OrZorgQuery]):
+class ZorgSQLRepo(QueryRepo[str, ZorgFile, OrZorgQuery]):
     """Repo that stores zorg notes in sqlite database."""
 
     def __init__(
@@ -23,7 +23,7 @@ class ZorgSQLRepo(QueryRepo[str, ZorgNote, OrZorgQuery]):
     ) -> None:
         self._session = session
 
-    def add(self, note: ZorgNote, /, *, key: str = None) -> ErisResult[str]:
+    def add(self, note: ZorgFile, /, *, key: str = None) -> ErisResult[str]:
         """Adds a new note to the DB.
 
         Returns a unique identifier that has been associated with this note.
@@ -31,19 +31,18 @@ class ZorgSQLRepo(QueryRepo[str, ZorgNote, OrZorgQuery]):
         del key
         self._session.add(note)
         # TODO(bugyi): Add ID generation logic here, which should add an event to the message bus.
-        assert note.ident is not None
-        return Ok(str(note.ident))
+        return Ok("")
 
     def remove(
-        self, note: ZorgNote, /  # noqa: W504
-    ) -> ErisResult[ZorgNote | None]:
+        self, note: ZorgFile, /  # noqa: W504
+    ) -> ErisResult[ZorgFile | None]:
         """Remove a note from the DB."""
         self._session.delete(note)
         return Ok(note)
 
-    def get(self, key: str) -> ErisResult[ZorgNote | None]:
+    def get(self, key: str) -> ErisResult[ZorgFile | None]:
         """Retrieve a note from the DB."""
-        stmt = select(sql.ZorgNote).where(sql.ZorgNote.id == int(key))
+        stmt = select(sql.ZorgFile).where(sql.ZorgFile.id == int(key))
         results = self._session.exec(stmt)
         sql_note = results.first()
         if sql_note:
@@ -51,14 +50,14 @@ class ZorgSQLRepo(QueryRepo[str, ZorgNote, OrZorgQuery]):
         else:
             return Ok(None)
 
-    def get_by_query(self, query: OrZorgQuery) -> ErisResult[list[ZorgNote]]:
+    def get_by_query(self, query: OrZorgQuery) -> ErisResult[list[ZorgFile]]:
         """Get note(s) from DB by using a query."""
         del query
         return Ok([])
 
-    def all(self) -> ErisResult[list[ZorgNote]]:
+    def all(self) -> ErisResult[list[ZorgFile]]:
         """Returns all zorg notes contained in the underlying SQL database."""
-        stmt = select(sql.ZorgNote)
+        stmt = select(sql.ZorgFile)
         return Ok([
             converters.sql_note_to_model(sql_note)
             for sql_note in self._session.exec(stmt).all()
