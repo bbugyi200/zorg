3c64ab0d28089e6a0b1a6723cb09fdb1db658bcd
Bryan Bugyi <bryanbugyi34@gmail.com>
2024-04-06 14:41:52 -0400

    Add conversion logic for properties

--- src/zorg/storage/sql/converters.py
+++ src/zorg/storage/sql/converters.py
@@ -42,6 +42,7 @@ class ZorgNoteConverter(EntityConverter[ZorgNote, sql.ZorgNote]):
     def __init__(self, session: Session) -> None:
         self._session = session
         self._tag_cache: dict[Any, dict[str, Any]] = defaultdict(lambda: {})
+        self._property_cache: dict[str, sql.Property] = {}
 
     def from_entity(self, entity: ZorgNote) -> sql.ZorgNote:
         """Model-to-SQL-model converter for a ZorgNote."""
@@ -73,6 +74,34 @@ class ZorgNoteConverter(EntityConverter[ZorgNote, sql.ZorgNote]):
                 tag = tag_cache[tag_name]
                 sql_tag_list.append(tag)
             setattr(sql_zorg_note, attr, sql_tag_list)
+
+        property_links = []
+        for k, v in entity.properties.items():
+            if k not in self._property_cache:
+                stmt = select(sql.Property).where(sql.Property.name == k)
+                results = self._session.exec(stmt)
+                prop = results.first()
+                if prop is None:
+                    prop = sql.Property(name=k)
+                self._property_cache[k] = prop
+
+            prop = self._property_cache[k]
+            stmt = (
+                select(sql.PropertyLink)
+                .where(sql.PropertyLink.note_id == sql_zorg_note.id)
+                .where(sql.PropertyLink.prop_id == prop.id)
+            )
+            results = self._session.exec(stmt)
+            prop_link = results.first()
+
+            if prop_link is None:
+                prop_link = sql.PropertyLink(
+                    prop=prop, todo=sql_zorg_note, value=v
+                )
+
+            property_links.append(prop_link)
+
+        sql_zorg_note.property_links = property_links
         return sql_zorg_note
 
     def to_entity(self, sql_model: sql.ZorgNote) -> ZorgNote:
