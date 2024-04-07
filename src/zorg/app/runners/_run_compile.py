e7d5ecc0ee8ec39d40758ad9b2a056ada67b6e09
Bryan Bugyi <bryanbugyi34@gmail.com>
2024-04-07 11:58:28 -0400

    Delete 'ident' field from zorg file dict

--- src/zorg/app/runners/_run_compile.py
+++ src/zorg/app/runners/_run_compile.py
@@ -2,7 +2,9 @@
 
 from dataclasses import asdict
 from pprint import pprint
+from typing import Any
 
+from ...domain.models import ZorgFile
 from ...service.compiler import walk_zorg_file
 from ..config import CompileConfig
 from ._runners import runner
@@ -12,8 +14,13 @@ from ._runners import runner
 def run_compile(cfg: CompileConfig) -> int:
     """Runner for the 'compile' command."""
     zorg_file = walk_zorg_file(cfg.zo_path)
-    zorg_file_dict = asdict(zorg_file)
-    del zorg_file_dict["path"]
-    del zorg_file_dict["messages"]
+    zorg_file_dict = _convert_zorg_file_to_dict(zorg_file)
     pprint(zorg_file_dict)
     return 0
+
+
+def _convert_zorg_file_to_dict(zorg_file: ZorgFile) -> dict[str, Any]:
+    zorg_file_dict = asdict(zorg_file)
+    for key in ["ident", "messages", "path"]:
+        del zorg_file_dict[key]
+    return zorg_file_dict
