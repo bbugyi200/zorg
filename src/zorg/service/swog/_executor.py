31d80b893a3b25316db89b825c5ab9e2fbc37241
Bryan Bugyi <bryanbugyi34@gmail.com>
2024-05-03 00:07:48 -0400

    Implement SELECT for notes

--- src/zorg/service/swog/_executor.py
+++ src/zorg/service/swog/_executor.py
@@ -1,16 +1,28 @@
 """Logic for executing zorg queries lives in this module."""
 
-from ...domain.types import NoteType
+from typist import assert_never
+
+from ...domain.types import NoteType, Select
 from ...storage.sql.session import SQLSession
 from ..compiler import build_zorg_query
 
 
-# Mapping from a note to its corresponding prefix symbol
-_NOTE_SYMBOL_MAP: dict[NoteType, str] = {NoteType}
-
-
 def _to_prefix_char(note_type: NoteType) -> str:
-    pass
+    if note_type is NoteType.BASIC:
+        return '-'
+    elif note_type is NoteType.OPEN_TODO:
+        return 'o'
+    elif note_type is NoteType.CLOSED_TODO:
+        return 'x'
+    elif note_type is NoteType.CANCELED_TODO:
+        return '~'
+    elif note_type is NoteType.BLOCKED_TODO:
+        return '<'
+    elif note_type is NoteType.PARENT_TODO:
+        return '>'
+    else:
+        assert_never(note_type)
+        
 
 
 def execute(session: SQLSession, query_string: str) -> str:
@@ -31,8 +43,20 @@ def execute(session: SQLSession, query_string: str) -> str:
     [[src/zorg/grammar/zorg_file/ZorgFile.g4]] grammar's "body" parser rule.
     """
     query = build_zorg_query(query_string)
+    result = ""
     with session:
         # (W)HERE
-        filtered_todos = session.repo.get_by_query(query.where)
+        filtered_notes = session.repo.get_by_query(query.where)
 
         # (S)ELECT
+        if query.select is Select.NOTES:
+            for note in filtered_notes:
+                char = _to_prefix_char(
+                    note.todo_payload.status
+                    if note.todo_payload
+                    else NoteType.BASIC
+                )
+                priority = f"{note.todo_payload.priority} " if note.todo_payload else ""
+                result += f"{char}{priority}{note.body}\n"
+
+    return result
