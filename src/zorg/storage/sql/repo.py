068eab6805c75f4ec3b90bbbc9dcbb04f287ba81
Bryan Bugyi <bryanbugyi34@gmail.com>
2024-05-09 01:07:27 -0400

    Remove unused imports

--- src/zorg/storage/sql/repo.py
+++ src/zorg/storage/sql/repo.py
@@ -7,7 +7,7 @@ from __future__ import annotations
 from pathlib import Path
 from typing import Optional
 
-from eris import ErisResult, Err, Ok
+from eris import ErisResult, Ok
 from logrus import Logger
 from sqlmodel import Session, select
 
