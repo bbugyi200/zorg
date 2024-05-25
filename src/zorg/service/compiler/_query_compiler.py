722939607a07a4bbf7a3232d23244f9bbc33008b
Bryan Bugyi <bryanbugyi34@gmail.com>
2024-05-25 11:34:48 -0400

    Stop logging warnings when subfilters are parsed

--- src/zorg/service/compiler/_query_compiler.py
+++ src/zorg/service/compiler/_query_compiler.py
@@ -169,7 +169,10 @@ class ZorgQueryCompiler(ZorgQueryListener):
                         value=value, op=desc_op, case_sensitive=case_sensitive
                     )
                 )
-            else:
+            # Subfilters are handled in a different method. See the
+            # enterSubfilter() and exitSubfilter() methods for more
+            # information.
+            elif not (w := where_atom.subfilter()):
                 _LOGGER.warning(
                     f"Unrecognized where atom: {where_atom.getText()}"
                 )
