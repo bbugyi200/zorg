f7f09a0f9fde3c2e023ca4a95b93ebc3bf6772be
Bryan Bugyi <bryanbugyi34@gmail.com>
2024-07-07 12:12:22 -0400

    Remove unused imports

--- tests/test_run_note.py
+++ tests/test_run_note.py
@@ -5,7 +5,6 @@ from __future__ import annotations
 from pathlib import Path
 from typing import Optional
 
-from _pytest.capture import CaptureFixture
 from pytest import mark, param
 from syrupy.assertion import SnapshotAssertion as Snapshot
 
